21dcd83891e4785de8caf4487d1c755442b76efc
Timothy Shu <tims11105@gmail.com>
2026-04-16 12:18:18 +1000

    retunned models under new regime

--- diamonds/main.py
+++ diamonds/main.py
@@ -37,6 +37,6 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     X_train, X_test = normalizeFeatures(X_train, X_test)
 
-    train(X_train, y_train, "models/diamonds", refit=True, hyperparameter_tuning_folds=3, trials=5)
+    train(X_train, y_train, "models/diamonds", refit=True, hyperparameter_tuning_folds=3, trials=50)
     
     test(X_test, y_test, "models/diamonds")
